9ced31a979fed567499c4ba96d4869cfe4945b8b
ptmertka <71412047+ptmertka@users.noreply.github.com>
2022-12-07 18:29:39 -0600

    Teleportation working and audio transition

--- ShiaLaBouef/Assets/Scripts/Scene1MovementScript.cs
+++ ShiaLaBouef/Assets/Scripts/Scene1MovementScript.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Scene1MovementScript : MonoBehaviour
 {
     public GameObject Rig;
     public float speed = 2.0f;
+    public AudioClip scene1Clip;
+    public AudioClip scene2Clip;
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<AudioSource>().loop = false;
+        StartCoroutine(playEngineSound());
         Rig = GameObject.FindWithTag("Player");
     }
 
@@ -16,5 +21,15 @@ public class Scene1MovementScript : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    IEnumerator playEngineSound()
+    {
+        GetComponent<AudioSource>().clip = scene1Clip;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length-1.1f);
+        Rig.transform.position = new Vector3(-24.73f, 1, 75);
+        GetComponent<AudioSource>().clip = scene2Clip;
+        GetComponent<AudioSource>().Play();
     }
 }
